e639ebcdd03a5aa339dc5d861cc328be094ebd2e
Nora Moreau <nora.moreau66@gmail.com>
2025-09-25 01:27:57 +0200

    <feat> : <Reconnaître l'épinglé, un tweet, un retweet, et étapes de suppression d'un tweet dans fonction supp()>

--- script_supptweet.js
+++ script_supptweet.js
@@ -1,7 +1,20 @@
+(() => {
+
+let pseudo = document.querySelector('[data-testid="UserName"] [tabindex="-1"]').innerText;
+console.log("Pseudo ", pseudo);
 
 function estEpingle(tweet) {
 	let socialContext = tweet.querySelector('[data-testid="socialContext"]');
 	return socialContext && socialContext.innerText == "Épinglé" ? true : false;
+}
+
+function estRetweet(tweet) {
+	let socialContext = tweet.querySelector('[data-testid="socialContext"]');
+	return socialContext && socialContext.innerText == "Vous avez reposté" ? true : false;
+}
+
+function estMonTweet(tweet) {
+	return tweet.querySelector('[data-testid="User-Name"] [tabindex="-1"]').innerText == pseudo ? true : false;
 }
 
 async function scrollBoucle() {
@@ -14,8 +27,20 @@ async function scrollBoucle() {
 			//Si c'est l'épinglé ne pas le toucher
 			if(estEpingle(tweet)) {
 				console.log("Ne pas toucher l épinglé");
+
+			//Si c'est un retweet annuler le retweet
+			} else if(estRetweet(tweet)) {
+				console.log("C'est un retweet");
+
+			//Si c'est un tweet à moi supprimer avec les trois petits points
+			} else if(estMonTweet(tweet)) {
+				console.log(tweet.querySelector('[data-testid="tweetText"]').innerText); //si texte bon, si photo erreur
+				let bouton = tweet.querySelector('[type="button"], [data-testid="caret"]');
+
+			//Un truc imprévu
 			} else {
-				console.log(tweet.querySelector('[data-testid="tweetText"]').innerText); 
+				console.log("IL Y A QUELQUE CHOSE");
+				console.log(tweet);
 			}
 		}
 		window.scrollBy(0, window.innerHeight*0.9);
@@ -24,4 +49,37 @@ async function scrollBoucle() {
 }
 
 scrollBoucle();
+
+})();
+
+
+async function suppTweet() { 
+	var tweet = document.querySelector('[data-testid="primaryColumn"] [data-testid="tweet"]');
+	console.log(tweet.querySelector('[data-testid="User-Name"] [tabindex="-1"]').innerText);
+	tweet.querySelector('[type="button"], [data-testid="caret"]').click();
+
+	await new Promise(r => setTimeout(r, 200)); //le temps que le menu apparaît
+
+	var menuDeroulantTweet = document.querySelector('[data-testid="Dropdown"]');
+	var suppBouton = menuDeroulantTweet.querySelector('[role="menuitem"] span');
+	console.log(suppBouton.innerText);
+	suppBouton.click();
+
+	await new Promise(r => setTimeout(r, 200)); //le temps que la fenêtre de confirmation apparaisse
+
+	var menuConfirmationSuppresion = document.querySelector('[data-testid="confirmationSheetDialog"]');
+	var confirmationSuppBouton = menuConfirmationSuppresion.querySelector('[data-testid="confirmationSheetConfirm"]');
+	console.log(confirmationSuppBouton.innerText);
+
+}
+
+suppTweet();
+
+
+async function annuleRetweet() {
+
+}
+
+annuleRetweet();
+
 
